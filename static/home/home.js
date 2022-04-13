df9c085fc30c1b02cbd64ebab1749658623f0871
daviddung1993 <david29101000@gmail.com>
2022-04-13 18:35:10 +0200

    redesign homepage

---
+++ static/home/home.js
@@ -0,0 +1,15 @@
+$(function(){
+    $(".card").on("mouseover", function(event) {
+        video = this.childNodes[1]
+        video.muted = true;
+        video.play();
+    }).on('mouseleave', function(event) {
+        if(video.id == "trading_ani"){
+            video.currentTime = 5;
+        } else {
+            video.currentTime = 0;
+        }
+        video.playbackRate = 1;
+        video.pause();
+    });
+})
